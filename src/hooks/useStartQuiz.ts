4bda4b7ebdbbfcd753ceb52e919b688ebc454458
Intigam Mammadli <intigam.mammadli@gmail.com>
2025-09-18 22:13:12 +0400

    Remove unused useStartQuiz hook and related imports

--- src/hooks/useStartQuiz.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import quizCardsStore from "../store/quizCardsStore";
-
-const useStartQuiz = () => {
-    const quizCards = quizCardsStore((state) => state.cards);
-};
-
-export default useStartQuiz;
